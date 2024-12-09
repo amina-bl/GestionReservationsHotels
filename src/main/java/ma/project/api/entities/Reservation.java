f25c82e03fd2b82038bb0d5eefa62873f14b96cb
Fatiha <Fatiha.Elbahraouy@emsi-edu.ma>
2024-12-09 12:51:04 +0100

    api soap

--- src/main/java/ma/project/api/entities/Reservation.java
+++ src/main/java/ma/project/api/entities/Reservation.java
@@ -2,38 +2,60 @@ package ma.project.api.entities;
 
 import java.util.Date;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import jakarta.persistence.*;
+import jakarta.xml.bind.annotation.*;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
-
-@Data
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Reservation {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	
-	@Temporal(TemporalType.DATE)
-	private Date dateDebut;
-	
-	@Temporal(TemporalType.DATE)
-	private Date dateFin;
-	
-	@ManyToOne
-	private Client client;
-	
-	@ManyToOne
-	private Chambre chambre;
-	
-	private String preferences;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Temporal(TemporalType.DATE)
+    private Date dateDebut;
+    
+    @Temporal(TemporalType.DATE)
+    private Date dateFin;
+    
+    @ManyToOne
+    @XmlTransient
+    private Client client;
+    
+    @ManyToOne
+    @XmlTransient
+    private Chambre chambre;
+    
+    private String preferences;
+
+    @XmlElement(name = "clientId")
+    public Long getClientId() {
+        return client != null ? client.getId() : null;
+    }
+
+    @XmlElement(name = "chambreId")
+    public Long getChambreId() {
+        return chambre != null ? chambre.getId() : null;
+    }
+
+    @Override
+    public String toString() {
+        return "Reservation{" +
+               "id=" + id +
+               ", dateDebut=" + dateDebut +
+               ", dateFin=" + dateFin +
+               ", preferences='" + preferences + '\'' +
+               ", clientId=" + getClientId() +
+               ", chambreId=" + getChambreId() +
+               '}';
+    }
 }
--- src/main/java/ma/project/api/entities/Utilisateur.java
+++ src/main/java/ma/project/api/entities/Utilisateur.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,6 +16,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Utilisateur {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
