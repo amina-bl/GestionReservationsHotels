73ae24a441d9e8b04c02edc73f7230f2b960dfd7
aboulamaat <aminaboulamaat4@gmail.com>
2024-12-11 11:05:50 +0100

    resolve conflit

--- src/main/java/ma/project/api/dto/ReservationRequest.java
+++ src/main/java/ma/project/api/dto/ReservationRequest.java
@@ -2,41 +2,17 @@ package ma.project.api.dto;
 
 import java.util.Date;
 
-<<<<<<< HEAD
-import org.springframework.boot.context.properties.bind.ConstructorBinding;
-
-import lombok.AllArgsConstructor;
 import lombok.Data;
-
-@Data
-public class ReservationRequest {
-	private Date dateDebut;
-	private Date dateFin;
-	private String preferences;
-	private Long clientId;
-	private Long chambreId;
-	public ReservationRequest() {}
-
-	@ConstructorBinding
-	public ReservationRequest(Date dateDebut, Date dateFin, String preferences, Long clientId, Long chambreId) {
-	    this.dateDebut = dateDebut;
-	    this.dateFin = dateFin;
-	    this.preferences = preferences;
-	    this.clientId = clientId;
-	    this.chambreId = chambreId;
-	}
-
-	
-=======
+import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import lombok.Getter;
 import lombok.Setter;
-import ma.project.api.entities.Chambre;
-import ma.project.api.entities.Client;
+
 
 @Setter
 @Getter
+@Data
 public class ReservationRequest {
 	
 	
@@ -48,6 +24,12 @@ public class ReservationRequest {
     private String preferences;
     private Long clientId;
     private Long chambreId;
-    
->>>>>>> 501e55c10e546755b69244a9bee1ed954f3a8b59
+	@ConstructorBinding
+	public ReservationRequest(Date dateDebut, Date dateFin, String preferences, Long clientId, Long chambreId) {
+		this.dateDebut = dateDebut;
+		this.dateFin = dateFin;
+		this.preferences = preferences;
+		this.clientId = clientId;
+		this.chambreId = chambreId;
+	}
 }
